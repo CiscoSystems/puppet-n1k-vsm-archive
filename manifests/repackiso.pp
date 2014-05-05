c37ffd2abb7551294974e6007efb17a6530ab72c
cwchang <cwchang@ubuntu120402-vm1.(none)>
2014-05-05 09:46:03 -0700

    Removed unneeded pp scripts

--- manifests/repackiso.pp
+++
@@ -1,5 +0,0 @@
-class n1k_vsm::repackiso {
-  
-  # cwchang to turn on later
-  #$xx = generate('/usr/bin/env', '/usr/share/puppet/modules/n1k_vsm/bin/repackiso.py', "-i${n1k_vsm::isoimage}", "-d${n1k_vsm::domainid}", "-n${n1k_vsm::vsmname}", "-m${n1k_vsm::mgmtip}", "-s${n1k_vsm::mgmtnetmask}", "-g${n1k_vsm::mgmtgateway}", "-p${n1k_vsm::adminpasswd}", "-r${n1k_vsm::role}" , "-f/etc/puppet/files/${n1k_vsm::role}_repacked.iso")
-}
